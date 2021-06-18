f526b9642fc007d818bdf3efaab61a2d317c4fe1
Thiago <tnfredes@globo.com>
2021-06-17 22:32:42 -0300

    Funçoes

--- aulas/aula01.py
+++ aulas/aula01.py
@@ -1,108 +1,108 @@
-# Comentário
+# # Comentário
 
-"""
-DocString
-"""
-#Entrada e saída de dados
+# """
+# DocString
+# """
+# #Entrada e saída de dados
 
-texto = input("Texto mostrado ao usuário")
-print (texto)
+# texto = input("Texto mostrado ao usuário")
+# print (texto)
 
-# Variáveis - Servem para armazenar valores
+# # Variáveis - Servem para armazenar valores
 
-n_clientes = 150
-cliente01 ="Carlos"
-saldoCliente = 1.500
+# n_clientes = 150
+# cliente01 ="Carlos"
+# saldoCliente = 1.500
  
-# Tipos de Dados
+# # Tipos de Dados
 
-## Strings Str
+# ## Strings Str
 
-nome = "Textos ou String"
-nome2 = 'Textos ou String'
+# nome = "Textos ou String"
+# nome2 = 'Textos ou String'
 
-## Inteiros Int
+# ## Inteiros Int
 
-valor = 150
+# valor = 150
 
-## Float
+# ## Float
 
-valor2 = 150.50
+# valor2 = 150.50
 
-## Booleanos Bool (Comparações)
+# ## Booleanos Bool (Comparações)
 
-bool1 = True
-bool2 = False
+# bool1 = True
+# bool2 = False
 
-## Operadores
+# ## Operadores
 
-### Operadores Aritiméticos
+# ### Operadores Aritiméticos
 
-# Soma
+# # Soma
 
-1+1
+# 1+1
 
-# Substração
+# # Substração
 
-1-1
+# 1-1
 
-# Divisão
+# # Divisão
 
-4/2
+# 4/2
 
-# Multiplicação
+# # Multiplicação
 
-2*2
+# 2*2
 
-# Divisão Inteira (Pega o número inteiro de uma divisão fracionada)
+# # Divisão Inteira (Pega o número inteiro de uma divisão fracionada)
 
-4//3
+# 4//3
 
-# Módulo da Divisão (Resto da divisão)
+# # Módulo da Divisão (Resto da divisão)
 
-4%3
+# 4%3
 
-# Exponenciação
+# # Exponenciação
 
-3**5
+# 3**5
 
-# Operador de Operação
+# # Operador de Operação
 
-n1 = 50
-n1 = n1 + 30 
+# n1 = 50
+# n1 = n1 + 30 
 
-ou
+# ou
 
-n1 = 50
-n1 += 30
+# n1 = 50
+# n1 += 30
 
-# Comparação
+# # Comparação
 
-n1 == n2 (igual)
-n1 != n2 (diferente)
-n1 >= n2
-n1 <= n2
-n1 > n2
-n1 < n2
+# n1 == n2 (igual)
+# n1 != n2 (diferente)
+# n1 >= n2
+# n1 <= n2
+# n1 > n2
+# n1 < n2
 
-# Operadores Lógicos
+# # Operadores Lógicos
 
-n1 < n2 and n3 < n4 #("AND" precisa das 2 afirmações para dar verdadeiro)
+# n1 < n2 and n3 < n4 #("AND" precisa das 2 afirmações para dar verdadeiro)
 
-n1 < n2 or n3 > n4 #("OR" Se uma das informações for verdadeiro é apresentado verdadeiro)
+# n1 < n2 or n3 > n4 #("OR" Se uma das informações for verdadeiro é apresentado verdadeiro)
 
-not(n1 < n2) # ("NOT" inverte a saída)
+# not(n1 < n2) # ("NOT" inverte a saída)
 
-# Atribuição
+# # Atribuição
 
-n1 = 100 
-n1 += 100 # Soma a variável n1 + 100
-n1 -= 100 # Subtração da variável n1 por 100
-n1 *= 100 # Multiplica a variável n1 por 100
-n1 /= 100 # Divisão da a variável n1 por 100
-n1 //= 100 # Divisão inteira da variável n1 por 100
-n1 %= 100 # Resto da divisão da variável n1 por 100
-n1 **= 100 # Exponencial da variável n1 elevado a 100
+# n1 == 100 
+# n1 + 100 # Soma a variável n1 + 100
+# n1 - 100 # Subtração da variável n1 por 100
+# n1 * 100 # Multiplica a variável n1 por 100
+# n1 / 100 # Divisão da a variável n1 por 100
+# n1 // 100 # Divisão inteira da variável n1 por 100
+# n1 % 100 # Resto da divisão da variável n1 por 100
+# n1 ** 100 # Exponencial da variável n1 elevado a 100
 
 
 
--- exercicios/exercicio_funcao_aula04.py
+++ exercicios/exercicio_funcao_aula04.py
@@ -8,3 +8,20 @@ def parImpar(numero):
         print('É impar')
 
 parImpar(numero)
+
+# Criar sistema de loja de roupas
+
+# O sistema conterá as seguintes funções?
+
+# verProduto -> lista com dicinários
+    #{nome: Camiseta Hulk, tamanho: M, preco: 390}
+
+# addProdutoCarrinho -> adiciona produto no carrinho
+
+# addDesconto -> se utilizado no carrinho terá 20% de desconto
+
+# removeProduto -> remove um produto do carrinho
+
+# checkout -> mostra total da compra
+
+# mainMenu -> Menu principal do sistema
